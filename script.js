463e67a83bc86f60a19683b105bd665a304108d4
Bradley Cantrell <cantrellbldev@gmail.com>
2020-08-12 21:12:20 -0400

    tweaks to logic, adding comments for clarity

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-const url = "https://mailchimp.zendesk.com/agent/tickets/7598235";
+
+//Accepts a string and returns any numbers 0-9 as a string, then combines the strings with no spaces:
+
+var url = "https://mailchimp.zendesk.com/agent/tickets/7598235";
 
 const clipped = new RegExp("[0-9]", "g");
 
 const zd = url.match(clipped);
 
+const type = typeof zd;
+console.log(type);
 
 console.log(zd.join(''));
+
+
+//Adjust to:
+//array of urls
+//regex on each url (for each?)
+//.match clipped on each url
+//return or log zd.join to return each clipped separated by commas 
