efc5cead5c62959472378e268a65a3e4d5570372
Bradley Cantrell <cantrellbldev@gmail.com>
2020-08-13 21:12:29 -0400

    adding headings, style changes

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-var urls = "https://mailchimp.zendesk.com/agent/tickets/7602662, https://mailchimp.zendesk.com/agent/tickets/7602662, https://mailchimp.zendesk.com/agent/tickets/7602670, www.google.com";
 
-
-var zds = urls.match(/\d{7}/g);
-
-
-for(i = 0; i < zds.length;i++) {
-      console.log(zds[i]);
-    };
+        var urls = prompt("Input comma separated ZD URLs:");
+        var zds = urls.match(/\d{7}/g);
+        document.write(zds);
